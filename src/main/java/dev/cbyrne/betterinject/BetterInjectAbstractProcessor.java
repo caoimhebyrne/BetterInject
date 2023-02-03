1c41e0752adf351899bce8fab7ccd11fc57defe0
Caoimhe Byrne <caoimhebyrne06@gmail.com>
2023-02-03 17:50:51 +0000

    AP: Fix crash when using latest loom version

--- src/main/java/dev/cbyrne/betterinject/BetterInjectAbstractProcessor.java
+++ src/main/java/dev/cbyrne/betterinject/BetterInjectAbstractProcessor.java
@@ -1,5 +1,7 @@
 package dev.cbyrne.betterinject;
 
+import org.spongepowered.asm.util.logging.MessageRouter;
+
 import javax.annotation.processing.AbstractProcessor;
 import javax.annotation.processing.ProcessingEnvironment;
 import javax.annotation.processing.RoundEnvironment;
@@ -13,6 +15,9 @@ public class BetterInjectAbstractProcessor extends AbstractProcessor {
     @Override
     public synchronized void init(ProcessingEnvironment processingEnv) {
         super.init(processingEnv);
+
+        // Mumfrey said this is required in the SpongePowered discord a few months ago, so we don't question it
+        MessageRouter.setMessager(processingEnv.getMessager());
         BetterInject.initialize();
     }
 
